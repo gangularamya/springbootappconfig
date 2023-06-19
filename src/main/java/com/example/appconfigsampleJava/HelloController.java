bfedfa735ddfb90df749da2b4eb7cabe97155a3b
System Administrator <admin@MngEnvMCAP477866.onmicrosoft.com>
2023-06-19 17:12:59 +0000

    Updated HelloController.java with keyvault code

--- src/main/java/com/example/appconfigsampleJava/HelloController.java
+++ src/main/java/com/example/appconfigsampleJava/HelloController.java
@@ -38,7 +38,7 @@ public class HelloController {
     @GetMapping("/message")
     @ResponseBody
     public String getMessage() {
-        return "Message: " + properties.getMessage() + "\n" + "Message1:" + properties.getMessage1();
+        return "Message: " + properties.getMessage() + "\n" + "Message1:" + properties.getMessage1()+ "\nKey Vault message: " + properties.getKeyVaultMessage();
     }
 
 }
